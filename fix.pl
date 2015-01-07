28b874a8a6d5d577986dadd7f4542dc1963248a7
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2015-01-07 10:58:10 +0100

    Orte.ttl aus JSP aktualisiert und beide synchronisiert

--- fix.pl
+++ fix.pl
@@ -2,8 +2,9 @@ undef $/;
 open(FH,shift) or die;
 $_=<FH>;
 
-s|leipzig-data.de/Data/Adresse/(\d+)\.|leipzig-data.de/Data/$1.Leipzig.|gs;
-s|leipzig-data.de/Data/ExterneAdresse/|leipzig-data.de/Data/|gs;
+s|leipzig-data.de/Data/(\d+)\.|leipzig-data.de/Data/$1.Leipzig.|gs;
+#s|leipzig-data.de/Data/Adresse/(\d+)\.|leipzig-data.de/Data/$1.Leipzig.|gs;
+#s|leipzig-data.de/Data/ExterneAdresse/|leipzig-data.de/Data/|gs;
 # s|rdfs:label "|rdfs:label "Leipzig, |gs;
 
 print;
