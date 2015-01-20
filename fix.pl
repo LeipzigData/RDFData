ba96db86bf05d683facc5b3ebb0e0ec28471419a
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2015-01-20 14:31:24 +0100

    Adressen in Orte.ttl gefixt

--- fix.pl
+++ fix.pl
@@ -3,9 +3,10 @@ open(FH,shift) or die;
 $_=<FH>;
 
 #s|leipzig-data.de/Data/(\d+)\.|leipzig-data.de/Data/$1.Leipzig.|gs;
-s|leipzig-data.de/Data/Adresse/(\d+)\.|leipzig-data.de/Data/$1.Leipzig.|gs;
+#s|leipzig-data.de/Data/Adresse/(\d+)\.|leipzig-data.de/Data/$1.Leipzig.|gs;
 #s|leipzig-data.de/Data/ExterneAdresse/|leipzig-data.de/Data/|gs;
-# s|rdfs:label "|rdfs:label "Leipzig, |gs;
+#s|rdfs:label "|rdfs:label "Leipzig, |gs;
+s|ld:hasURL "([^"]+)"|ld:hasURL <$1>|gs;
 
 print;
 
