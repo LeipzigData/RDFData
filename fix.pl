2b12beba6a82633563ba6f60697fb5febb5f83d1
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2015-08-19 11:59:20 +0200

    Ämter aus dem IHR-15 Projekt übernommen

--- fix.pl
+++ fix.pl
@@ -6,7 +6,7 @@ $_=<FH>;
 #s|leipzig-data.de/Data/Adresse/(\d+)\.|leipzig-data.de/Data/$1.Leipzig.|gs;
 #s|leipzig-data.de/Data/ExterneAdresse/|leipzig-data.de/Data/|gs;
 #s|rdfs:label "|rdfs:label "Leipzig, |gs;
-s|ld:hasURL "([^"]+)"|ld:hasURL <$1>|gs;
-
+#s|ld:hasURL "([^"]+)"|ld:hasURL <$1>|gs;
+s|ld:hasStadtRef "(\S+)"|ld:hasStadtRef <http://leipzig-data.de/Data/StadtId/Amt.$1>|gs;
 print;
 
